c3cd62742cf1d0a4769ecb2e60124239a39e96ae
majunjie <1197173994@qq.com>
2022-11-20 01:43:04 +0800

    feat:init project

---
+++ micro-message/src/main/java/com/pony/baas/message/Main.java
@@ -0,0 +1,14 @@
+package com.pony.baas.message;
+
+/**
+ * @author majunjie
+ * @version 1.0
+ * @project micro-messagemicro-message
+ * @description
+ * @date 2022/11/20 01:10:47
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
